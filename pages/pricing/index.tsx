c81d04750683b427f576592534025afe0f6e6fc3
Edgard Alas <edgardalasu@gmail.com>
2022-04-16 17:38:33 -0600

    fix en ruta en pantalla

--- pages/pricing/index.tsx
+++ pages/pricing/index.tsx
@@ -10,7 +10,8 @@ export default function Pricing() {
       </h1>
 
       <p className={'description'}>
-        Get started by editing <code className={'code'}>pages/home.js</code>
+        Get started by editing{' '}
+        <code className={'code'}>pages/pricing/index.js</code>
       </p>
     </MainlLayout>
   );
